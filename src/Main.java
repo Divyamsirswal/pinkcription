63851bd0a87b7ac67bb68a593882b90aafd8933d
divyamTest <divyamsirswal361@gmail.com>
2024-04-11 12:25:36 +0530

    updates the file User.java and InputValidation.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		try {
+			Scanner sc = new Scanner(System.in);
 
+			
 		} catch (Exception e) {
 
 		}
--- src/User.java
+++ src/User.java
@@ -1,9 +1,24 @@
+
 public class User {
   private String user_name;
   private String user_pwd;
+  private String file_ = "path/gonna_update_later";
 
-  public User(String name, String pwd) {
+  public User(String name, String pwd, String file_) {
     this.user_name = name;
     this.user_pwd = pwd;
+    this.file_ = file_;
+  }
+
+  public String get_user_name() {
+    return user_name;
+  }
+
+  public String get_user_pwd() {
+    return user_pwd;
+  }
+
+  public String get_file_path(){
+    return file_;
   }
 }
