65085810dd90dde6b1264b615796c56ec13af430
divyamTest <divyamsirswal361@gmail.com>
2024-04-11 12:37:02 +0530

    changes in the file

--- src/InputValidation.java
+++ src/InputValidation.java
@@ -2,5 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class InputValidation {
-  
+  private static final String valid_user_name = "^[a-zA-Z0-9_-]{5,8}$";
+  private static final String valid_user_pwd = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{5,8}$";
+
+  public static boolean isValidUsername(String username) {
+    return Pattern.matches(USERNAME_REGEX, username);
+  }
+
+  public static boolean isValidPassword(String password) {
+    return Pattern.matches(PASSWORD_REGEX, password);
+  }
+
 }
