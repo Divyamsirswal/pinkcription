3607561ccfa4121f7153a75dd1c0a652676628d5
divyamTest <divyamsirswal361@gmail.com>
2024-04-11 12:12:50 +0530

    changes in the structure

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+  private String user_name;
+  private String user_pwd;
+
+  public User(String name, String pwd) {
+    this.user_name = name;
+    this.user_pwd = pwd;
+  }
+}
