114dfbb63446239f919e6e507886c7edc521741f
divyamTest <divyamsirswal361@gmail.com>
2024-04-11 22:49:34 +0530

    changes in the file

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,34 @@ public class Main {
         String file_;
 
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter username: ");
+            System.out.print("\n\n-------Enter username--------\n");
             name = sc.nextLine();
 
             if (!InputValidation.isValidUsername(name)) {
                 throw new IllegalArgumentException(
-                        "Invalid username: Username must be at least 5 and atmost 8 character long");
+                        "\nInvalid username: Username must be at least 5 and atmost 8 character long");
             }
 
-            System.out.print("Enter password: ");
+            System.out.print("\n\n-------Enter password--------\n");
             pwd = sc.nextLine();
 
             if (!InputValidation.isValidPassword(pwd)) {
                 throw new IllegalArgumentException(
-                        "Invalid password: Password must be at least 5 and atmost 8 character long");
+                        "\nInvalid password: Password must be at least 5 and atmost 8 character long");
             }
-
+            System.out.println("\n------Please fill your choice-----");
             System.out.println("1. Encrypt File");
             System.out.println("2. Decrypt File");
             int ask = sc.nextInt();
             sc.nextLine();
 
-            System.out.print("Enter file name: ");
+            System.out.print("\n------> Enter file name <------\n");
 
             file_ = sc.nextLine();
             Path path = Paths.get(file_);
 
             if (!(Files.exists(path))) {
-                throw new IllegalArgumentException("File does not exist: " + file_);
+                throw new IllegalArgumentException("\n------ File does not exist !!!! ------ \n" + file_);
             }
 
             User usr = new User(name, pwd, file_);
