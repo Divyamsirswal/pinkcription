9e408721e19bb6949f90327e1923beae6eb48a97
divyamTest <divyamsirswal361@gmail.com>
2024-04-11 12:48:34 +0530

    changes in the User.java file

--- src/User.java
+++ src/User.java
@@ -7,6 +7,21 @@ public class User {
   private String file_ = "path/gonna_update_later";
 
   public User(String name, String pwd, String file_) {
+    if (!InputValidation.isValidUsername(name)) {
+      throw new IllegalArgumentException(
+          "Invalid username ");
+    }
+    if (!InputValidation.isValidPassword(pwd)) {
+      throw new IllegalArgumentException(
+          "Invalid password ");
+    }
+    if (file_ == null || file_.isEmpty()) {
+      throw new IllegalArgumentException("File path cannot be null or empty.");
+    }
+
+    if (!Files.exists(Paths.get(file_))) {
+      throw new IllegalArgumentException("File does not exist.");
+    }
 
     this.user_name = name;
     this.user_pwd = pwd;
